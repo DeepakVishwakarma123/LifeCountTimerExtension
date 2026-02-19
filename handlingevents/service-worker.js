416ea2559beb1ab46fc092f4fc762f97cb35bc44
DeepakLinux <deepakvishwakarmajs@gmail.com>
2026-02-19 23:45:51 +0530

    extension building guide chrome tools

---
+++ handlingevents/service-worker.js
@@ -0,0 +1,45 @@
+import './sw-omnibox.js';
+import './sw-tips.js';
+
+// Save default API suggestions
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === 'install') {
+    chrome.storage.local.set({
+      test: ['tabs', 'storage', 'scripting']
+    });
+  }
+});
+
+
+
+const URL_CHROME_EXTENSIONS_DOC =
+  'https://developer.chrome.com/docs/extensions/reference/';
+const NUMBER_OF_PREVIOUS_SEARCHES = 4;
+
+// Display the suggestions after user starts typing
+chrome.omnibox.onInputChanged.addListener(async (input, suggest) => {
+  await chrome.omnibox.setDefaultSuggestion({
+    description: 'Enter a Chrome API or choose from past searches'
+  });
+  const { test } = await chrome.storage.local.get(["test"]);
+  const suggestions = test.map((api) => {
+    return { content: api, description: `Open chrome.${api} API` };
+  });
+  suggest(suggestions);
+});
+
+
+// Open the reference page of the chosen API
+chrome.omnibox.onInputEntered.addListener((input) => {
+  chrome.tabs.create({ url: URL_CHROME_EXTENSIONS_DOC + input });
+  // Save the latest keyword
+  updateHistory(input);
+});
+
+
+async function updateHistory(input) {
+  const { test } = await chrome.storage.local.get(["test"]);
+  test.unshift(input);
+  test.splice(NUMBER_OF_PREVIOUS_SEARCHES);
+  return chrome.storage.local.set({ test });
+}
